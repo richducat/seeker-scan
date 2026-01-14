ca264e42c6e1489cad730764842f89b6791323df
richducat <richducat@gmail.com>
2026-01-14 12:22:46 -0800

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-// Render the root application into the #root element. Using React.StrictMode
-// helps identify potential issues in the application during development.
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+
+const network = 'https://api.mainnet-beta.solana.com';
+const wallets = [new PhantomWalletAdapter()];
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ConnectionProvider endpoint={network}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <App />
+      </WalletProvider>
+    </ConnectionProvider>
   </React.StrictMode>
 );
